81070e0dfafa590c577ee8725a966a588b0d37d2
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-06-27 17:17:03 +0200

    Fix lint

--- services/table-analyzer.js
+++ services/table-analyzer.js
@@ -1,4 +1,3 @@
-'use strict';
 const _ = require('lodash');
 const P = require('bluebird');
 
@@ -23,6 +22,8 @@ function TableAnalyzer(queryInterface, config) {
       case 'sqlite':
         query = `PRAGMA foreign_key_list('${table}');`;
         break;
+      default:
+        break;
     }
 
     return queryInterface.sequelize
@@ -42,28 +43,29 @@ function TableAnalyzer(queryInterface, config) {
 
     return queryInterface.sequelize
       .query(query, { type: queryInterface.sequelize.QueryTypes.SELECT })
-      .then((result) => !!result.length);
+      .then(result => !!result.length);
   }
 
   async function getType(columnInfo, columnName) {
     const { type, special } = columnInfo;
 
     switch (type) {
-      case 'BIT': // MSSQL type
+      case 'BIT': // MSSQL type
       case 'BOOLEAN':
         return 'BOOLEAN';
       case 'CHARACTER VARYING':
       case 'TEXT':
-      case 'NTEXT': // MSSQL type
+      case 'NTEXT': // MSSQL type
       case (type.match(/TEXT.*/i) || {}).input:
       case (type.match(/VARCHAR.*/i) || {}).input:
       case (type.match(/CHAR.*/i) || {}).input:
-      case 'NVARCHAR': // MSSQL type
+      case 'NVARCHAR': // MSSQL type
         return 'STRING';
-      case 'USER-DEFINED':
+      case 'USER-DEFINED': {
         const isEnum = await checkIfColumnIsTypeEnum(columnName);
 
         return isEnum ? `ENUM('${special.join('\', \'')}')` : 'STRING';
+      }
       case 'UNIQUEIDENTIFIER':
       case 'UUID':
         return 'UUID';
@@ -83,7 +85,7 @@ function TableAnalyzer(queryInterface, config) {
       case 'REAL':
       case 'DOUBLE PRECISION':
       case (type.match(/DECIMAL.*/i) || {}).input:
-      case 'MONEY': // MSSQL type
+      case 'MONEY': // MSSQL type
         return 'DOUBLE';
       case 'DATE':
       case 'DATETIME':
@@ -91,27 +93,28 @@ function TableAnalyzer(queryInterface, config) {
       case 'TIMESTAMP WITH TIME ZONE':
       case 'TIMESTAMP WITHOUT TIME ZONE':
         return 'DATE';
+      default:
+        return null;
     }
   }
 
-  this.analyzeTable = function (table) {
-    return new P
-      .all([analyzeFields(table), analyzeForeignKeys(table)])
+  this.analyzeTable = function analyzeTable(table) {
+    return P.all([analyzeFields(table), analyzeForeignKeys(table)])
       .spread(async (schema, foreignKeys) => {
-        var fields = [];
-        var references = [];
+        const fields = [];
+        const references = [];
 
         await P.each(Object.keys(schema), async (columnName) => {
           const columnInfo = schema[columnName];
           const type = await getType(columnInfo, columnName);
-          let foreignKey = _.find(foreignKeys, { 'column_name': columnName });
+          const foreignKey = _.find(foreignKeys, { column_name: columnName });
 
           if (foreignKey && foreignKey.foreign_table_name &&
             foreignKey.column_name && !columnInfo.primaryKey) {
-            let ref = {
+            const ref = {
               ref: foreignKey.foreign_table_name,
               foreignKey: foreignKey.column_name,
-              as: `_${foreignKey.column_name}`
+              as: `_${foreignKey.column_name}`,
             };
 
             if (foreignKey.foreign_column_name !== 'id') {
@@ -120,7 +123,7 @@ function TableAnalyzer(queryInterface, config) {
 
             references.push(ref);
           } else if (type && columnName !== 'id') {
-            var opts = { name: columnName, type: type, primaryKey: columnInfo.primaryKey };
+            const opts = { name: columnName, type, primaryKey: columnInfo.primaryKey };
             fields.push(opts);
           }
         });
