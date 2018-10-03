2a0e5b4b7404610898ef99a0d659ce50cae0f610
Sandro Munda <sandro@munda.me>
2018-10-03 17:38:06 +0200

    Use the authenticator.logout function in the logout command

--- lumber-logout.js
+++ lumber-logout.js
@@ -3,18 +3,10 @@ const os = require('os');
 const fs = require('fs');
 const chalk = require('chalk');
 const logger = require('./services/logger');
+const authenticator = require('./services/authenticator');
 
 program
   .description('Sign out of your account.')
   .parse(process.argv);
 
-const path = `${os.homedir()}/.lumberrc`;
-
-fs.stat(path, (err) => {
-  if (err === null) {
-    fs.unlinkSync(path);
-    console.log(chalk.green('👍  You\'re now unlogged 👍 '));
-  } else if (err.code === 'ENOENT') {
-    logger.error('🔥  You\'re not logged 🔥');
-  }
-});
+await authenticator.logout({ log: true });
