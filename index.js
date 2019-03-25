0be95bd19e7329c9749592df3090e0716a192e8b
Josh Egan <josh.egan@lambdaschool.com>
2019-03-25 15:16:47 -0600

    allow programmatic use of the authenticator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const generate = require('./app/generate');
 const update = require('./app/update');
+const authenticator = require('./services/authenticator');
 
 module.exports = {
+  authenticator,
   generate,
   update,
 };
