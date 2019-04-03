024c2e1c739f2c8b2335687a2e1c80cf5b4f39de
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-04-03 14:57:05 +0200

    [-] Wording - Replace all existing mentions of "back office" by "admin panel" or "admin panel API" (#210)
    * [-] Wording - Replace all existing mentions of "back office" by "admin panel" or "admin panel API"
    
    * Fix the rebase

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@
 - Opn - Completely remove the opn NPM package.
 - Dependencies - Fix vulnaribilities by upgrading the dependencies in the package.json.
 - Command Generate - Make the appName argument mandatory from the command line args.
+- Wording - Replace all existing mentions of "back office" by "admin panel" or "admin panel API".
 
 ### Fixed
 - Command Update - Fix the ssl environment variable.
--- lumber-deploy.js
+++ lumber-deploy.js
@@ -2,7 +2,7 @@ const program = require('commander');
 const chalk = require('chalk');
 
 program
-  .description('Deploy your back office to your production environment.')
+  .description('Deploy your admin panel API to your production environment.')
   .parse(process.argv);
 
 console.log(`Read the instructions: \n ${chalk.green('https://docs.forestadmin.com/lumber/getting-started/setup-guide#deploy-to-production')}`);
