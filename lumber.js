7d49341adb87570e7be2d91535bfd48871f90fc8
Sandro Munda <sandro@munda.me>
2019-04-09 14:11:02 +0200

    Remove commands related to Forest Admin

--- lumber.js
+++ lumber.js
@@ -12,8 +12,4 @@ program
   .version(packagejson.version)
   .command('generate <projectName>', 'generate a GraphQL API backend')
   .command('update', 'update your models\'s definition according to your database schema')
-  .command('deploy', 'deploy your admin panel application to production')
-  .command('user', 'show your current logged user')
-  .command('login <email>', 'sign in to your Forest account')
-  .command('logout', 'sign out of your Forest account')
   .parse(process.argv);
