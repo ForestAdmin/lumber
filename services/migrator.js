cbeba0299eff555faae6d06df8be35bc083da731
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-08-08 15:47:47 +0200

    Fix the rebase

--- services/migrator.js
+++ services/migrator.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const _ = require('lodash');
 const chalk = require('chalk');
 const logger = require('./logger');
-const { hasTimestamps, isUnderscored, getModelName } = require('../utils/model-generator-helper');
+const { getModelName } = require('../utils/model-generator-helper');
 
 function Migrator(config) {
   this.createModel = (schema, table) => {
