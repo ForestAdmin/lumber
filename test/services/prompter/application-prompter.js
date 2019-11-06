c888ee47c0f931af18532e4721962d764df9f168
Steve Bunlon <bunlon.steve@gmail.com>
2019-11-06 16:23:56 +0100

    Add application prompter tests

--- test/services/prompter/application-prompter.js
+++ test/services/prompter/application-prompter.js
@@ -1,149 +1,174 @@
-const { describe, it } = require('mocha');
+const {
+  describe,
+  it,
+  after,
+  before,
+} = require('mocha');
 const { expect } = require('chai');
 const ApplicationPrompts = require('../../../services/prompter/application-prompts');
 
-/*describe('Services > Prompter > Application prompts', () => {
+const FAKE_APP_HOST = 'fakeApplicationHost';
+const FAKE_APP_PORT = '1234';
+
+describe('Services > Prompter > Application prompts', () => {
+  let envConfig = {};
+  let requests = [];
+  let program = {};
+  let prompts = [];
+
+  function resetParams() {
+    envConfig = {};
+    requests = [];
+    program = {};
+    prompts = [];
+  }
+
   describe('Handling host name : ', () => {
     describe('When the appHostName option is requested', () => {
-      const envConfig = {};
-      const requests = ['appHostName'];
-      const program = {};
-      const prompts = [];
+      describe('and the appHostName has not been passed in', async () => {
+        before(() => {
+          requests.push('appHostname');
+        });
 
-      describe('and the projectName has not been passed in', () => {
-        const projectPrompts = new ProjectPrompts(undefined, envConfig, requests);
+        after(() => {
+          resetParams();
+        });
 
-        it('should terminate the process with exit code 1', async () => {
-          await projectPrompts.handleName();
+        const projectPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
+
+        await projectPrompts.handleHostName();
+
+        it('should add a prompt to ask for for it', () => {
+          expect(prompts).to.have.lengthOf(1);
+        });
+
+        it('should add a prompt with the correct configuration', () => {
+          expect(prompts[0].type).to.equal('input');
+          expect(prompts[0].name).to.equal('appHostname');
+          expect(prompts[0].message).to.equal('What\'s the IP/hostname on which your application will be running? ');
+          expect(prompts[0].default).to.equal('localhost');
+        });
+
+        it('should not change the configuration', () => {
+          expect(envConfig.appHostname).to.equal(undefined);
         });
       });
 
-      describe('and the projectName has already been passed in', () => {
-        const projectPrompts = new ProjectPrompts(FAKE_PROJECT_NAME, envConfig, requests);
-
-        describe('and the directory to write in is not available', async () => {
-          before(() => {
-            const mkdirAsync = P.promisify(mkdir);
-
-            mkdirAsync.sync(`${process.cwd()}/${TEST_TMP_DIRECTORY}/${FAKE_PROJECT_NAME}`);
-          });
-
-          after(async () => {
-            const rmdirAsync = P.promisify(rmdir);
-
-            await rmdirAsync(`${process.cwd()}/${TEST_TMP_DIRECTORY}`);
-          });
-
-          it('should terminate the process with exit code 1', async () => {
-            await projectPrompts.handleName();
-          });
-
-          it('should log an error in the console', async () => {
-            // TODO HANDLE LOGGER
-          });
-
-          it('should send an event', async () => {
-            // TODO HANDLE EVENT SENDING
-          });
+      describe('and the appHostName has already been passed in', async () => {
+        before(() => {
+          requests.push('appHostname');
         });
 
-        describe('and the directory to write in is available', async () => {
-          await projectPrompts.handleName();
+        after(() => {
+          resetParams();
+        });
 
-          it('should add the appName to the configuration', async () => {
-            expect(envConfig.appName).to.equal(undefined);
+        program.applicationHost = FAKE_APP_HOST;
 
-            await projectPrompts.handleName();
+        const applicationPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
 
-            expect(envConfig.appName).to.equal(FAKE_PROJECT_NAME);
-          });
+        await applicationPrompts.handleHostName();
+
+        it('should not add any prompt', () => {
+          expect(prompts).to.have.lengthOf(0);
+        });
+
+        it('should add the appHostname to the configuration', () => {
+          expect(envConfig.appHostname).to.equal(FAKE_APP_HOST);
         });
       });
     });
 
-    describe('When the appHostName option is not requested', () => {
-      const projectPrompts = new ApplicationPrompts(FAKE_PROJECT_NAME, envConfig, requests);
+    describe('When the appHostname option is not requested', () => {
+      const projectPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
 
       it('should not do anything', async () => {
-        expect(envConfig.appName).to.equal(undefined);
+        expect(envConfig.appHostname).to.equal(undefined);
+        expect(prompts).to.have.lengthOf(0);
 
-        await projectPrompts.handleName();
+        await projectPrompts.handleHostName();
 
-        expect(envConfig.appName).to.equal(undefined);
+        expect(envConfig.appHostname).to.equal(undefined);
+        expect(prompts).to.have.lengthOf(0);
       });
     });
   });
 
   describe('Handling port : ', () => {
-    describe('when the appName option is requested', () => {
-      const envConfig = {};
-      const requests = ['appName'];
-      const exitFunction = process.exit;
+    describe('When the appPort option is requested', () => {
+      describe('and the appPort has not been passed in', async () => {
+        before(() => {
+          requests.push('appPort');
+        });
 
-      describe('and the projectName has not been passed in', () => {
-        const projectPrompts = new ProjectPrompts(undefined, envConfig, requests);
+        after(() => {
+          resetParams();
+        });
 
-        it('should terminate the process with exit code 1', async () => {
-          await projectPrompts.handleName();
+        const projectPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
+
+        await projectPrompts.handleAppPort();
+
+        it('should add a prompt to ask for for it', () => {
+          expect(prompts).to.have.lengthOf(1);
+        });
+
+        it('should add a prompt with the correct configuration', () => {
+          expect(prompts[0].type).to.equal('input');
+          expect(prompts[0].name).to.equal('appPort');
+          expect(prompts[0].message).to.equal('What\'s the port on which your application will be running? ');
+          expect(prompts[0].default).to.equal('3310');
+          expect(prompts[0].validate).to.be.a('function');
+        });
+
+        it('should validate the port', () => {
+          expect(prompts[0].validate(FAKE_APP_PORT)).to.equal(true);
+          expect(prompts[0].validate('non number port')).to.equal('The port must be a number.');
+          expect(prompts[0].validate(70000)).to.equal('This is not a valid port.');
+        });
+
+        it('should not change the configuration', () => {
+          expect(envConfig.appPort).to.equal(undefined);
         });
       });
 
-      describe('and the projectName has already been passed in', () => {
-        const projectPrompts = new ProjectPrompts(FAKE_PROJECT_NAME, envConfig, requests);
-
-        describe('and the directory to write in is not available', async () => {
-          before(() => {
-            const mkdirAsync = P.promisify(mkdir);
-
-            mkdirAsync.sync(`${process.cwd()}/${TEST_TMP_DIRECTORY}/${FAKE_PROJECT_NAME}`);
-          });
-
-          after(async () => {
-            const rmdirAsync = P.promisify(rmdir);
-
-            await rmdirAsync(`${process.cwd()}/${TEST_TMP_DIRECTORY}`);
-          });
-
-          it('should terminate the process with exit code 1', async () => {
-            await projectPrompts.handleName();
-          });
-
-          it('should log an error in the console', async () => {
-            // TODO HANDLE LOGGER
-          });
-
-          it('should send an event', async () => {
-            // TODO HANDLE EVENT SENDING
-          });
+      describe('and the appPort has already been passed in', async () => {
+        before(() => {
+          requests.push('appPort');
         });
 
-        describe('and the directory to write in is available', async () => {
-          await projectPrompts.handleName();
+        after(() => {
+          resetParams();
+        });
 
-          it('should add the appName to the configuration', async () => {
-            expect(envConfig.appName).to.equal(undefined);
+        program.applicationHost = FAKE_APP_PORT;
 
-            await projectPrompts.handleName();
+        const applicationPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
 
-            expect(envConfig.appName).to.equal(FAKE_PROJECT_NAME);
-          });
+        await applicationPrompts.handleHostName();
+
+        it('should not add any prompt', () => {
+          expect(prompts).to.have.lengthOf(0);
+        });
+
+        it('should add the appPort to the configuration', () => {
+          expect(envConfig.appPort).to.equal(FAKE_APP_PORT);
         });
       });
     });
 
-    describe('when the appName option is not requested', () => {
-      const envConfig = {};
-      const requests = [];
-
-      const projectPrompts = new ProjectPrompts(FAKE_PROJECT_NAME, envConfig, requests);
+    describe('When the appPort option is not requested', () => {
+      const projectPrompts = new ApplicationPrompts(program, envConfig, prompts, requests);
 
       it('should not do anything', async () => {
-        expect(envConfig.appName).to.equal(undefined);
+        expect(envConfig.appPort).to.equal(undefined);
+        expect(prompts).to.have.lengthOf(0);
 
-        await projectPrompts.handleName();
+        await projectPrompts.handleHostName();
 
-        expect(envConfig.appName).to.equal(undefined);
+        expect(envConfig.appPort).to.equal(undefined);
+        expect(prompts).to.have.lengthOf(0);
       });
     });
   });
-});*/
+});
