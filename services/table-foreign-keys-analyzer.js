4bb921e7d96a9cbe466cd2c8923073034f0025be
DrRaider <nsailly@live.fr>
2019-11-21 15:32:34 +0100

    HasMany and hasOne logic

--- services/table-foreign-keys-analyzer.js
+++ services/table-foreign-keys-analyzer.js
@@ -8,20 +8,40 @@ function TableForeignKeysAnalyzer(databaseConnection, schema) {
     switch (queryInterface.sequelize.options.dialect) {
       case 'postgres':
         query = `
-          SELECT
-            tc.constraint_name,
+        SELECT tc.constraint_name,
             tc.table_name,
             tc.constraint_type AS column_type,
             kcu.column_name,
             ccu.table_name AS foreign_table_name,
-            ccu.column_name AS foreign_column_name
+            ccu.column_name AS foreign_column_name,
+            json_agg(uidx.unique_indexes) filter (where uidx.unique_indexes is not null) AS unique_indexes
           FROM information_schema.table_constraints AS tc
           JOIN information_schema.key_column_usage AS kcu
             ON tc.constraint_name = kcu.constraint_name
           JOIN information_schema.constraint_column_usage AS ccu
             ON ccu.constraint_name = tc.constraint_name
-          WHERE (constraint_type = 'FOREIGN KEY' OR constraint_type = 'UNIQUE')
-            AND tc.table_name=:table;`;
+          FULL OUTER JOIN (select
+                ix.indexrelid::regclass as index_name,
+                t.relname as table_name,
+                json_agg(DISTINCT a.attname) as unique_indexes
+            from
+                pg_class t,
+                pg_class i,
+                pg_index ix,
+                pg_attribute a,
+                pg_namespace ns
+            where
+                t.oid = ix.indrelid
+                and i.oid = ix.indexrelid
+                and a.attrelid = t.oid
+                and a.attnum = ANY(ix.indkey)
+                and not ix.indisprimary
+                and ix.indisunique
+                and t.relkind = 'r'
+                and not t.relname like 'pg%'
+            group by table_name, index_name) as uidx on uidx.table_name = tc.table_name
+            WHERE tc.table_name=:table
+          group by tc.constraint_name, tc.table_name, tc.constraint_type, kcu.column_name, foreign_table_name, foreign_column_name`;
         break;
       case 'mysql':
         query = `
