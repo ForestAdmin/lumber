71058e040d0c6f9f1722411f382bd10419a3e8c7
Steve Bunlon <bunlon.steve@gmail.com>
2019-11-26 11:22:09 +0100

    Restrict BIT(1) for mysql only and add tests

--- test/fixtures/mysql/customers.sql
+++ test/fixtures/mysql/customers.sql
@@ -3,6 +3,7 @@ create table customers (
   name varchar(255) not null,
   description text null,
   is_active tinyint(1) default 1 not null,
+  paying bit(1) default b'0',
   created_at date not null,
   updated_at date null
 );
--- test/fixtures/postgres/customers.sql
+++ test/fixtures/postgres/customers.sql
@@ -3,6 +3,7 @@ create table customers (
   name varchar not null,
   description text,
   is_active boolean default true not null,
+  paying bit(1) default B'0',
   created_at timestamp not null,
   updated_at timestamp
 );
