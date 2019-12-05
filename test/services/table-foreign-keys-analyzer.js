5c7ae89945cc57407f32fca323fb635e2b0d47aa
DrRaider <nsailly@live.fr>
2019-12-05 17:09:09 +0100

    Added expected file for sql queries

--- test/services/table-foreign-keys-analyzer.js
+++ test/services/table-foreign-keys-analyzer.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const SequelizeHelper = require('../utils/sequelize-helper');
 const { databases } = require('../utils/databases-urls');
 const TableForeignKeysAnalyzer = require('../../services/table-foreign-keys-analyzer');
+const expectedData = require('../expected/table-foreign-keys-analyzer-output');
 
 describe('Table analyser > SQL', () => {
   databases.forEach(({ connectionUrl, dialect }) => {
@@ -22,34 +23,8 @@ describe('Table analyser > SQL', () => {
       it('should provide the constraints of a table', async () => {
         const tableForeignKeysAnalyzer = new TableForeignKeysAnalyzer(databaseConnection, 'public');
         const constraints = await tableForeignKeysAnalyzer.perform('addresses');
-        const expectedConstraints = {
-          postgres: [
-            'constraint_name',
-            'table_name',
-            'column_type',
-            'column_name',
-            'foreign_table_name',
-            'foreign_column_name',
-            'unique_indexes',
-          ].sort(),
-          mysql: [
-            'constraint_name',
-            'table_name',
-            'column_name',
-            'foreign_table_name',
-            'foreign_column_name',
-          ],
-          mssql: [
-            'constraint_name',
-            'table_name',
-            'column_name',
-            'foreign_table_name',
-            'foreign_column_name',
-          ],
-        };
 
-        constraints.forEach((constraint) =>
-          expect(Object.keys(constraint).sort()).to.eql(expectedConstraints[dialect].sort()));
+        expect(constraints).is.deep.equal(expectedData[dialect]);
       });
     });
   });
