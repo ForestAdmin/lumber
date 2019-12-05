2521475c91e2a4bf70a5ba601e61dc3e6e08b838
DrRaider <nsailly@live.fr>
2019-12-05 16:01:55 +0100

    updated test

--- test/services/table-foreign-keys-analyzer.js
+++ test/services/table-foreign-keys-analyzer.js
@@ -22,29 +22,34 @@ describe('Table analyser > SQL', () => {
       it('should provide the constraints of a table', async () => {
         const tableForeignKeysAnalyzer = new TableForeignKeysAnalyzer(databaseConnection, 'public');
         const constraints = await tableForeignKeysAnalyzer.perform('addresses');
+        const expectedConstraints = {
+          postgres: [
+            'constraint_name',
+            'table_name',
+            'column_type',
+            'column_name',
+            'foreign_table_name',
+            'foreign_column_name',
+            'unique_indexes',
+          ].sort(),
+          mysql: [
+            'constraint_name',
+            'table_name',
+            'column_name',
+            'foreign_table_name',
+            'foreign_column_name',
+          ],
+          mssql: [
+            'constraint_name',
+            'table_name',
+            'column_name',
+            'foreign_table_name',
+            'foreign_column_name',
+          ],
+        };
 
-        constraints.forEach((constraint) => {
-          // Temporary 'if' while we don't have all the databases scripts updated
-          if (dialect === 'postgres') {
-            expect(Object.keys(constraint).sort()).to.eql([
-              'constraint_name',
-              'table_name',
-              'column_type',
-              'column_name',
-              'foreign_table_name',
-              'foreign_column_name',
-              'unique_indexes',
-            ].sort());
-          } else {
-            expect(Object.keys(constraint).sort()).to.eql([
-              'constraint_name',
-              'table_name',
-              'column_name',
-              'foreign_table_name',
-              'foreign_column_name',
-            ].sort());
-          }
-        });
+        constraints.forEach((constraint) =>
+          expect(Object.keys(constraint).sort()).to.eql(expectedConstraints[dialect].sort()));
       });
     });
   });
