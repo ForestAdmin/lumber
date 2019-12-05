795acb4bb4712a5c4ff8b36ac2721e7453add44d
DrRaider <nsailly@live.fr>
2019-12-05 16:36:28 +0100

    Added Unique constraint test

--- test/fixtures/postgres/addresses.sql
+++ test/fixtures/postgres/addresses.sql
@@ -1,5 +1,5 @@
 create table addresses (
   id uuid not null constraint addresses_pk primary key,
   customer_id int constraint addresses_customers_id_fk references customers,
-  city varchar not null
+  city varchar not null unique
 );
