570c2163f6dafbd5db2ebc039e31e04b3d4fecac
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-12-09 09:10:02 +0100

    Fix

--- test/utils/sequelize-helper.js
+++ test/utils/sequelize-helper.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 class SequelizeHelper {
-  async connect(url) {
+  connect(url) {
     this.sequelize = new Sequelize(url, {
       logging: false,
       pool: { maxConnections: 10, minConnections: 1 },
