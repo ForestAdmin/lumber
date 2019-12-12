9325ee46de5c5b5cbe576ead7a29dbe775a21451
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-12-12 08:56:13 +0100

    Remove useless files

--- test/fixtures/mysql/books.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE books (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  author VARCHAR(100) NOT NULL,
-  published_date DATE NOT NULL,
-  isbn INT,
-  CONSTRAINT books_isbn_key UNIQUE (isbn ASC)
-);
--- test/fixtures/postgres/books.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE books (
-  id SERIAL,
-  title VARCHAR(100) NOT NULL,
-  author VARCHAR(100) NOT NULL,
-  published_date TIMESTAMP NOT NULL,
-  isbn INT,
-  PRIMARY KEY (id),
-  UNIQUE (isbn)
-);
