88b1991c470f169474e42e27bcad46c1a0e4f8c6
Sliman <smedini@gmail.com>
2019-12-18 15:54:42 +0100

    Add test withs array of numbers and array of objects.

---
+++ test-fixtures/mongo/nested-array-of-numbers-model.js
@@ -0,0 +1,12 @@
+/* eslint no-underscore-dangle: off */
+const { ObjectID } = require('mongodb');
+
+const persons = [
+  {
+    _id: ObjectID(),
+    name: 'James Cameron',
+    propArrayOfNumbers: [1, 2, 3],
+  },
+];
+
+module.exports = { persons };
--- test/services/database-analyzer/database-analyzer-mongo.test.js
+++ test/services/database-analyzer/database-analyzer-mongo.test.js
@@ -7,12 +7,16 @@ const multipleReferencesModel = require('../../../test-fixtures/mongo/multiple-r
 const manyNullsModel = require('../../../test-fixtures/mongo/many-nulls-model');
 const complexModel = require('../../../test-fixtures/mongo/many-objectid-fields-model');
 const nestedObjectModel = require('../../../test-fixtures/mongo/nested-object-model');
+const nestedArrayOfObjectsModel = require('../../../test-fixtures/mongo/nested-array-of-objects-model');
+const nestedArrayOfNumbersModel = require('../../../test-fixtures/mongo/nested-array-of-numbers-model');
 const expectedSimpleModel = require('../../../test-expected/mongo/db-analysis-output/simple.expected.json');
 const expectedHasManyModel = require('../../../test-expected/mongo/db-analysis-output/hasmany.expected.json');
 const expectedMultipleReferencesModel = require('../../../test-expected/mongo/db-analysis-output/multiple-references-from-same-field.expected.json');
 const expectedManyuNullsModel = require('../../../test-expected/mongo/db-analysis-output/many-nulls.expected.json');
 const expectedManyObjectIDFieldsModel = require('../../../test-expected/mongo/db-analysis-output/many-objectid-fields.expected.json');
 const expectedNestedObjectModel = require('../../../test-expected/mongo/db-analysis-output/nested-object-fields.expected.json');
+const expectedNestedArrayOfObjectsModel = require('../../../test-expected/mongo/db-analysis-output/nested-array-of-objects-fields.expected.json');
+const expectedNestedArrayOfNumbersModel = require('../../../test-expected/mongo/db-analysis-output/nested-array-of-numbers-fields.expected.json');
 
 function getMongoHelper(mongoUrl) {
   return new MongoHelper(mongoUrl);
@@ -20,6 +24,13 @@ function getMongoHelper(mongoUrl) {
 
 describe('services > database analyser > MongoDB', () => {
   describeMongoDatabases((mongoUrl) => () => {
+    async function testAnalyser(fixture, expectedModel) {
+      await mongoHelper.given(fixture);
+      const databaseAnalyzer = new DatabaseAnalyzer(databaseConnection, { dbDialect: 'mongodb' });
+      const model = await databaseAnalyzer.perform();
+      expect(model).is.deep.equal(expectedModel);
+    }
+
     it('should connect and insert a document.', async () => {
       expect.assertions(1);
       const mongoHelper = await getMongoHelper(mongoUrl);
@@ -92,11 +103,8 @@ describe('services > database analyser > MongoDB', () => {
       await mongoHelper.close();
     });
 
-    it('should generate the model with a nested object', async () => {
-      await mongoHelper.given(nestedObjectModel);
-      const databaseAnalyzer = new DatabaseAnalyzer(databaseConnection, { dbDialect: 'mongodb' });
-      const model = await databaseAnalyzer.perform();
-      expect(model).is.deep.equal(expectedNestedObjectModel);
-    });
+    it('should generate the model with a nested object', () => testAnalyser(nestedObjectModel, expectedNestedObjectModel));
+    it('should generate the model with a nested array of numbers', () => testAnalyser(nestedArrayOfNumbersModel, expectedNestedArrayOfNumbersModel));
+    it('should generate the model with a nested array of objects', () => testAnalyser(nestedArrayOfObjectsModel, expectedNestedArrayOfObjectsModel));
   });
 });
