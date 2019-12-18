10eadc3c541a377ba49f9b49092ba1055c1e527a
Sliman <smedini@gmail.com>
2019-12-18 11:53:45 +0100

    Add a test with a simple nested object generation.

---
+++ test-fixtures/mongo/nested-object-model.js
@@ -0,0 +1,17 @@
+/* eslint no-underscore-dangle: off */
+const { ObjectID } = require('mongodb');
+
+const persons = [
+  {
+    _id: ObjectID(),
+    name: 'James Cameron',
+    propGroup: {
+      answer: false,
+      date: new Date(),
+      sentence: 'Life is beautiful',
+      number: 1664,
+    },
+  },
+];
+
+module.exports = { persons };
--- test/services/database-analyzer/database-analyzer-mongo.test.js
+++ test/services/database-analyzer/database-analyzer-mongo.test.js
@@ -6,11 +6,13 @@ const hasManyModel = require('../../../test-fixtures/mongo/hasmany-model');
 const multipleReferencesModel = require('../../../test-fixtures/mongo/multiple-references-same-field-model');
 const manyNullsModel = require('../../../test-fixtures/mongo/many-nulls-model');
 const complexModel = require('../../../test-fixtures/mongo/many-objectid-fields-model');
+const nestedObjectModel = require('../../../test-fixtures/mongo/nested-object-model');
 const expectedSimpleModel = require('../../../test-expected/mongo/db-analysis-output/simple.expected.json');
 const expectedHasManyModel = require('../../../test-expected/mongo/db-analysis-output/hasmany.expected.json');
 const expectedMultipleReferencesModel = require('../../../test-expected/mongo/db-analysis-output/multiple-references-from-same-field.expected.json');
 const expectedManyuNullsModel = require('../../../test-expected/mongo/db-analysis-output/many-nulls.expected.json');
 const expectedManyObjectIDFieldsModel = require('../../../test-expected/mongo/db-analysis-output/many-objectid-fields.expected.json');
+const expectedNestedObjectModel = require('../../../test-expected/mongo/db-analysis-output/nested-object-fields.expected.json');
 
 function getMongoHelper(mongoUrl) {
   return new MongoHelper(mongoUrl);
@@ -88,6 +90,13 @@ describe('services > database analyser > MongoDB', () => {
       const model = await databaseAnalyzer.perform();
       expect(model).toStrictEqual(expectedManyObjectIDFieldsModel);
       await mongoHelper.close();
+    });
+
+    it('should generate the model with a nested object', async () => {
+      await mongoHelper.given(nestedObjectModel);
+      const databaseAnalyzer = new DatabaseAnalyzer(databaseConnection, { dbDialect: 'mongodb' });
+      const model = await databaseAnalyzer.perform();
+      expect(model).is.deep.equal(expectedNestedObjectModel);
     });
   });
 });
