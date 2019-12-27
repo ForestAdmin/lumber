746af9abb30e1dfe8888e902a99591d131bf352f
Sliman <smedini@gmail.com>
2019-12-27 14:25:45 +0100

    [+] Technical - Add sonarJS linter for complexity issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,19 @@ module.exports = {
   root: true,
   extends: [
     'airbnb-base',
-    'plugin:jest/all'
+    'plugin:jest/all',
+    "plugin:sonarjs/recommended"
   ],
-  plugins: [],
+  plugins: [
+    "sonarjs"
+  ],
   env: {
     mocha: true,
   },
   rules: {
+    'sonarjs/no-identical-functions': 0,
+    'sonarjs/no-duplicate-string': 0,
+    'sonarjs/no-same-line-conditional': 0,
     'implicit-arrow-linebreak': 0,
     'import/no-extraneous-dependencies': [
       'error',
