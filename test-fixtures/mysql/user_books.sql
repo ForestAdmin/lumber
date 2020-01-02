a1a435e4d6db64be6dc7288f8673be896ee9cc2a
Vincent Molinié <vincent.molinie@epita.fr>
2020-01-02 14:35:55 +0100

    Fix tests

--- test-fixtures/mysql/user_books.sql
+++ test-fixtures/mysql/user_books.sql
@@ -1,9 +1,8 @@
 CREATE TABLE user_books (
   user_id INT NOT NULL,
   book_id INT NOT NULL,
-  checkout_date DATE,
-  return_date DATE,
-  CONSTRAINT user_books_pkey PRIMARY KEY (user_id, book_id),
+  creation_date DATE,
+  updated_date DATE,
   CONSTRAINT user_books_book_id_fkey FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT user_books_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
--- test-fixtures/postgres/user_books.sql
+++ test-fixtures/postgres/user_books.sql
@@ -1,9 +1,8 @@
 CREATE TABLE user_books (
   user_id INT NOT NULL,
   book_id INT NOT NULL,
-  checkout_date TIMESTAMP,
-  return_date TIMESTAMP,
-  PRIMARY KEY (user_id, book_id),
+  creation_date TIMESTAMP,
+  updated_date TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE,
   FOREIGN KEY (book_id) REFERENCES books(id) ON UPDATE CASCADE
 );
