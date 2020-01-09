cf122210a0ff5d7250de0d3c8be31553df3c515a
slimane <slimanm@forestadmin.com>
2020-01-09 16:37:45 +0100

    names in test

--- test/services/mysql-table-constraints-getter.test.js
+++ test/services/mysql-table-constraints-getter.test.js
@@ -10,12 +10,12 @@ describe('services > mysql table constraints getter', () => {
     it('should provide an empty unique constraint array', async () => {
       expect.assertions(1);
       const fixture = [
-        { columnType: 'BLABLA', constraintName: 'FOOFOO1' },
-        { columnType: 'BLABLA', constraintName: 'FOOFOO2' },
-        { columnType: 'BLABLA', constraintName: 'FOOFOO3' },
+        { columnType: 'PRIMARY_KEY', constraintName: 'PRIMARY' },
+        { columnType: 'FOREIGN_KEY', constraintName: 'test_fkey' },
+        { columnType: 'FOREIGN_KEY', constraintName: 'test_fkey' },
       ];
-      const tested = new MysqlTableConstraintsGetter(databaseConnectionMock);
-      const actual = tested.convertToUniqueIndexArray(fixture);
+      const constraintGetter = new MysqlTableConstraintsGetter(databaseConnectionMock);
+      const actual = constraintGetter.convertToUniqueIndexArray(fixture);
       const expected = null;
 
       expect(actual).toStrictEqual(expected);
@@ -25,13 +25,13 @@ describe('services > mysql table constraints getter', () => {
     it('should provide an unique constraint array', async () => {
       expect.assertions(1);
       const fixture = [
-        { columnType: 'BLABLA', constraintName: 'FOOFOO1' },
-        { columnType: 'UNIQUE', constraintName: 'FOOFOO2', columnName: 'one' },
-        { columnType: 'BLABLA', constraintName: 'FOOFOO3' },
-        { columnType: 'UNIQUE', constraintName: 'FOOFOO4', columnName: 'anotherOne' },
+        { columnType: 'PRIMARY_KEY', constraintName: 'PRIMARY' },
+        { columnType: 'UNIQUE', constraintName: 'one_unique_index', columnName: 'one' },
+        { columnType: 'FOREIGN_KEY', constraintName: 'test_fkey' },
+        { columnType: 'UNIQUE', constraintName: 'anotherOne_unique_index', columnName: 'anotherOne' },
       ];
-      const tested = new MysqlTableConstraintsGetter(databaseConnectionMock);
-      const actual = tested.convertToUniqueIndexArray(fixture);
+      const constraintGetter = new MysqlTableConstraintsGetter(databaseConnectionMock);
+      const actual = constraintGetter.convertToUniqueIndexArray(fixture);
       const expected = [['one'], ['anotherOne']];
 
       expect(actual).toStrictEqual(expected);
@@ -41,14 +41,14 @@ describe('services > mysql table constraints getter', () => {
     it('should provide an unique constraint array', async () => {
       expect.assertions(1);
       const fixture = [
-        { columnType: 'BLABLA', constraintName: 'FOOFOO1' },
-        { columnType: 'UNIQUE', constraintName: 'FOOFOO2', columnName: 'left' },
-        { columnType: 'BLABLA', constraintName: 'FOOFOO3' },
-        { columnType: 'UNIQUE', constraintName: 'FOOFOO4', columnName: 'right' },
-        { columnType: 'UNIQUE', constraintName: 'FOOFOO4', columnName: 'up' },
+        { columnType: 'PRIMARY_KEY', constraintName: 'PRIMARY' },
+        { columnType: 'UNIQUE', constraintName: 'left_unique_index', columnName: 'left' },
+        { columnType: 'FOREIGN_KEY', constraintName: 'test_fkey' },
+        { columnType: 'UNIQUE', constraintName: 'right_up_unique_index', columnName: 'right' },
+        { columnType: 'UNIQUE', constraintName: 'right_up_unique_index', columnName: 'up' },
       ];
-      const tested = new MysqlTableConstraintsGetter(databaseConnectionMock);
-      const actual = tested.convertToUniqueIndexArray(fixture);
+      const constraintGetter = new MysqlTableConstraintsGetter(databaseConnectionMock);
+      const actual = constraintGetter.convertToUniqueIndexArray(fixture);
       const expected = [['left'], ['right', 'up']];
 
       expect(actual).toStrictEqual(expected);
