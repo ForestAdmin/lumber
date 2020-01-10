07cadc6063ec4991a34ab2da7b6aa161f4960c18
Raphaël Huchet <raphaelht@gmail.com>
2020-01-10 14:14:34 +0100

    fix

--- utils/to-valid-package-name.js
+++ utils/to-valid-package-name.js
@@ -1,20 +1,23 @@
 const validate = require('validate-npm-package-name');
 
 module.exports = function toValidPackageName(packageName) {
-  const { validForNewPackages, validForOldPackages } = validate(packageName);
-  const isValid = validForNewPackages && validForOldPackages;
+  function isValid(name) {
+    const { validForNewPackages, validForOldPackages } = validate(name);
+    return validForNewPackages && validForOldPackages;
+  }
 
-  if (!isValid) {
+  if (!isValid(packageName)) {
     // NOTICE: Create an always valid package name (disallow almost everything)
-    return packageName.toLowerCase()
+    const validPackageName = packageName.toLowerCase()
       // Remove all non "a-z", "0-9", "-" characters with hyphen.
       .replace(/[^a-z0-9\\-]/g, '-')
       // Remove hyphen sequence (> 1).
       .replace(/-{2,}/g, '-')
       // Remove leading and trailing hyphen.
-      .replace(/^-|-$/g, '')
-      // If empty, replace with my-lumber-project.
-      .replace(/^-*$/g, 'my-lumber-project');
+      .replace(/^-|-$/g, '');
+
+    // NOTICE: Return 'lumber-project' if sanitized package name is still not valid.
+    return isValid(validPackageName) ? validPackageName : 'lumber-project';
   }
 
   return packageName;
