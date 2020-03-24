dd67c588aad765e3c2888c30127456fc7900f3eb
Guillaume Deslandes <guillaumed@forestadmin.com>
2020-03-24 15:19:20 +0100

    [+] String Helper - Add reserved word checker and use in transformToSafeString
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- utils/strings.js
+++ utils/strings.js
@@ -1,5 +1,24 @@
 const _ = require('lodash');
 
+const reservedWords = [
+  'abstract', 'await', 'boolean', 'break',
+  'byte', 'case', 'catch', 'char',
+  'class', 'const', 'continue', 'debugger',
+  'default', 'delete', 'do', 'double',
+  'else', 'enum', 'export', 'extends',
+  'false', 'final', 'finally', 'float',
+  'for', 'function', 'goto', 'if',
+  'implements', 'import', 'in', 'instanceof',
+  'int', 'interface', 'let', 'long',
+  'native', 'new', 'null', 'package',
+  'private', 'protected', 'public', 'return',
+  'short', 'static', 'super', 'switch',
+  'synchronized', 'this', 'throw', 'throws',
+  'transient', 'true', 'try', 'typeof',
+  'undefined', 'var', 'void', 'volatile',
+  'while', 'with', 'yield',
+];
+
 module.exports = {
   pascalCase(input) {
     return _.chain(input).camelCase().upperFirst().value();
@@ -9,9 +28,17 @@ module.exports = {
     return _.camelCase(input);
   },
 
+  isReservedWord(input) {
+    return reservedWords.includes(_.toLower(input));
+  },
+
   transformToSafeString(input) {
     if (/^[\d]/g.exec(input)) {
       return `model${input}`;
+    }
+    // Notice: add dash to get proper snake/pascal case
+    if (this.isReservedWord(input)) {
+      return `model-${input}`;
     }
     return input;
   },
