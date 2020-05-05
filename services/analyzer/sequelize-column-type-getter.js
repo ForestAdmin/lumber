b7094f4392fbabe58ba328537b39c2ca446e7dd9
Steve Bunlon <bunlon.steve@gmail.com>
2020-05-05 21:26:55 +0200

    chore(sonar): remove unecessary cognitive-complexity warning

--- services/analyzer/sequelize-column-type-getter.js
+++ services/analyzer/sequelize-column-type-getter.js
@@ -77,6 +77,7 @@ function ColumnTypeGetter(databaseConnection, schema, allowWarning = true) {
     return `ARRAY(DataTypes.${await this.perform(innerColumnInfo, innerColumnInfo.udtName, tableName)})`;
   };
 
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   this.perform = async (columnInfo, columnName, tableName) => {
     const { type } = columnInfo;
 
