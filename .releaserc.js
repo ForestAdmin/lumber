64dbf878ce610d2f6144ffb76eec1e421929ee95
Arnaud Besnier <arnaudibesnier@gmail.com>
2020-05-11 18:41:19 +0200

    chore(build): use semantic release to publish the new versions (#426)

---
+++ .releaserc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github',
+    [
+      'semantic-release-slack-bot',
+      {
+        markdownReleaseNotes: true,
+        notifyOnSuccess: true,
+        notifyOnFail: false,
+        onSuccessTemplate: {
+          text: "👾 $package_name@$npm_package_version has been released!",
+          blocks: [{
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: '*New `$package_name` package released!*'
+            }
+          }, {
+            type: 'context',
+            elements: [{
+              type: 'mrkdwn',
+              text: "👾  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>"
+            }]
+          }, {
+            type: 'divider',
+          }],
+          attachments: [{
+            blocks: [{
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: '*Changes* of version $release_notes',
+              },
+            }],
+          }],
+        },
+        packageName: 'lumber-cli',
+      }
+    ],
+  ],
+}
--- scripts/release.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config(); // eslint-disable-line
-const { ReleaseManager, ReleaseNoteManager } = require('@forestadmin/devops'); // eslint-disable-line
-
-const { DEVOPS_SLACK_TOKEN, DEVOPS_SLACK_CHANNEL } = process.env;
-const OPTIONS = { releaseIcon: '👾', withVersion: true };
-
-new ReleaseManager(OPTIONS).create()
-  .then(() => new ReleaseNoteManager(DEVOPS_SLACK_TOKEN, DEVOPS_SLACK_CHANNEL, OPTIONS).create());
