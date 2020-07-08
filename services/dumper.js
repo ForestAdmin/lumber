954ad30fe04ecd0b3f55cbcccadc9f16b83026ac
SteveBunlon <SteveBunlon@users.noreply.github.com>
2020-07-08 17:13:14 +0200

    fix(dumper): fix issue where project using nodejs v14 could never connect to postgres database (#450)

--- services/dumper.js
+++ services/dumper.js
@@ -83,7 +83,7 @@ function Dumper(config) {
 
     if (config.dbDialect) {
       if (config.dbDialect.includes('postgres')) {
-        dependencies.pg = '~6.1.0';
+        dependencies.pg = '~8.2.2';
       } else if (config.dbDialect === 'mysql') {
         dependencies.mysql2 = '~1.7.0';
       } else if (config.dbDialect === 'mssql') {
