518745a7edd40b606b08cace8ecc0c2ff5937938
jeffladiray <ladirayjeff@gmail.com>
2020-07-27 15:45:55 +0200

    ci(commitlint): dynamically estimate max header length for pull request title on squash and merge

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,19 @@
+// NOTICE: When a github "squash and merge" is performed, github add the PR link in the commit
+//         message using the format ` (#<PR_ID>)`. Travis provide the target branch of the build,
+//         so authorizing 4+5 = 9 characters more on master for the max header length should work
+//         until we reach PR #99999.
+
+let maxHeaderLength = 100;
+
+const prExtrasChars = 9;
+
+const isCommitOnMaster = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH === 'master';
+
+if (isCommitOnMaster) {
+  maxHeaderLength += prExtrasChars;
+}
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'header-max-length': [2, 'always', 100] },
+  rules: { 'header-max-length': [2, 'always', maxHeaderLength] },
 };
