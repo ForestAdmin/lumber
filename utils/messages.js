1bbba8c5aa4bfccde5a2be76f9824cc1b1816dc3
Guillaume Gautreau <guillaume+github@ghusse.com>
2020-08-03 14:23:11 +0200

    fix: suggest to reach help in the discourse community instead of Slack (#456)
    * fix: suggest to reach help in the discourse community instead of Slack
    
    * fix: change discourse to developpers community

--- utils/messages.js
+++ utils/messages.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ERROR_UNEXPECTED: 'An unexpected error occurred. Please reach out for help in our Slack community or create a Github issue with following error:',
+  ERROR_UNEXPECTED: 'An unexpected error occurred. Please reach out for help in our Developers community (https://community.forestadmin.com/) or create a Github issue with following error:',
   ERROR_MISSING_PROJECT_NAME: 'Missing project name in the command.',
   HINT_MISSING_PROJECT_NAME: 'Please specify a project name. Type lumber help for more information.',
   ERROR_NOT_PARSABLE_CONNECTION_URL: 'Cannot parse the database dialect. Please, check the syntax of the database connection string.',
