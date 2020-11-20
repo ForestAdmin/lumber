84248d3520f7df6eab47436b283a8ef95aa45a29
Guillaume Deslandes <guillaumed@forestadmin.com>
2020-11-20 12:09:30 +0100

    test(models): fix date types
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test-fixtures/mysql/books.sql
+++ test-fixtures/mysql/books.sql
@@ -2,7 +2,7 @@ CREATE TABLE books (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   author VARCHAR(100) NOT NULL,
-  published_date DATE NOT NULL,
+  published_date DATETIME NOT NULL,
   isbn INT,
   CONSTRAINT books_isbn_key UNIQUE (isbn ASC)
 );
