3529a09be4eee1c056910f84b0736d788960e7bd
Guillaume Gautreau <guillaumeg@forestadmin.com>
2020-12-09 16:46:38 +0100

    style: use a constant instead of repeating the same string

--- test/services/application-token.unit.test.js
+++ test/services/application-token.unit.test.js
@@ -1,6 +1,8 @@
 const ApplicationTokenService = require('../../services/application-token');
 const UnableToCreateApplicationTokenError = require('../../utils/errors/application-token/unable-to-create-application-token-error');
 
+const SESSION_TOKEN = 'SESSION-TOKEN';
+
 describe('services > ApplicationToken', () => {
   function setup() {
     const os = {
@@ -27,13 +29,13 @@ describe('services > ApplicationToken', () => {
       os.hostname.mockReturnValue('Machine name');
       api.createApplicationToken.mockReturnValue({ token: 'ABCDE' });
 
-      const result = await applicationTokenService.generateApplicationToken('SESSION-TOKEN');
+      const result = await applicationTokenService.generateApplicationToken(SESSION_TOKEN);
 
       expect(result).toStrictEqual('ABCDE');
       expect(os.hostname).toHaveBeenCalledWith();
       expect(api.createApplicationToken).toHaveBeenCalledWith({
         name: 'Lumber @Machine name',
-      }, 'SESSION-TOKEN');
+      }, SESSION_TOKEN);
     });
 
     it('should throw an error if something goes wrong with the API', async () => {
@@ -44,7 +46,7 @@ describe('services > ApplicationToken', () => {
       os.hostname.mockReturnValue('Machine name');
       api.createApplicationToken.mockRejectedValue(new Error('Internal error'));
 
-      await expect(applicationTokenService.generateApplicationToken('SESSION-TOKEN'))
+      await expect(applicationTokenService.generateApplicationToken(SESSION_TOKEN))
         .rejects.toBeInstanceOf(UnableToCreateApplicationTokenError);
     });
   });
