7804dda12c0bcac5366c3b08af9549ac845e931d
Guillaume Gautreau <guillaumeg@forestadmin.com>
2020-12-11 16:10:51 +0100

    fix: initialize dependencies before running the command generate

--- services/project-creator.js
+++ services/project-creator.js
@@ -9,6 +9,8 @@ const { ERROR_UNEXPECTED } = require('../utils/messages');
 
 if (!api) throw new Error('Missing dependency api');
 
+if (!api) throw new Error('Missing dependency api');
+
 function ProjectCreator(sessionToken) {
   this.createProject = async (projectName, config) => {
     try {
