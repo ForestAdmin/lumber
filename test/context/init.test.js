1c859b1afe83a4fd4e93530b29921cc9c9c00221
jeffladiray <ladirayjeff@gmail.com>
2020-12-15 17:18:30 +0100

    fix: review fix

--- test/context/init.test.js
+++ test/context/init.test.js
@@ -5,17 +5,6 @@ describe('context > init', () => {
   it('should not throw error with an empty context', () => {
     expect.assertions(1);
 
-    initContext(new ApplicationContext());
-
     expect(() => initContext(new ApplicationContext())).not.toThrow();
-  });
-
-  it('should throw an error if the provided context already have a defined key', () => {
-    expect.assertions(1);
-
-    const ctxt = new ApplicationContext();
-    ctxt.addInstance('logger', {});
-
-    expect(() => initContext(ctxt)).toThrow("existing instance { key: 'logger'} in context");
   });
 });
