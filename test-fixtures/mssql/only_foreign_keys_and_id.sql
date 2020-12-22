3280d5fdbbae58ca72b5604a148d77b67aef5eb3
Steve Bunlon <bunlon.steve@gmail.com>
2020-12-22 12:25:13 +0100

    fix(sql): fix mssql query

--- test-fixtures/mssql/only_foreign_keys_and_id.sql
+++ test-fixtures/mssql/only_foreign_keys_and_id.sql
@@ -1,8 +1,8 @@
 CREATE TABLE only_foreign_keys_and_id (
     id INT PRIMARY KEY,
-    customer_id BIGINT,
+    sample_id INT,
     car_id INT,
-    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id)  REFERENCES customers (id),
+    CONSTRAINT fk_sample_id FOREIGN KEY (sample_id)  REFERENCES sample_table (id),
     CONSTRAINT fk_car_id FOREIGN KEY (car_id)  REFERENCES cars (id)
 );
 
