2983ee9fb62b186fee845fb0e3609baa36db5b16
jeffladiray <ladirayjeff@gmail.com>
2020-12-29 16:03:24 +0100

    refactor: now use error handler instead of custom code

--- lumber-update.js
+++ lumber-update.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
-const chalk = require('chalk');
 const program = require('commander');
-const fs = require('fs');
-const path = require('path');
 const DatabaseAnalyzer = require('./services/analyzer/database-analyzer');
 const spinners = require('./services/spinners');
-const { updateErrors, LumberError } = require('./utils/errors');
-const { ERROR_UNEXPECTED } = require('./utils/messages');
+const { updateErrors } = require('./utils/errors');
 const context = require('./context');
 const initContext = require('./context/init');
 
@@ -18,7 +14,13 @@ program
   .option('-o, --output-directory <output-directory-path>', 'the output directory to export new files into')
   .parse(process.argv);
 
-const { database, dumper, terminator } = context.inject();
+const {
+  database,
+  dumper,
+  errorHandler,
+  fs,
+  path,
+} = context.inject();
 
 (async () => {
   const options = {
@@ -71,14 +73,5 @@ const { database, dumper, terminator } = context.inject();
 
   process.exit(0);
 })().catch(async (error) => {
-  const logs = ['Cannot update your project.'];
-  if (error instanceof LumberError) {
-    logs.push(error.message);
-  } else {
-    logs.push(`${ERROR_UNEXPECTED} ${chalk.red(error)}`);
-  }
-
-  await terminator.terminate(1, {
-    logs,
-  });
+  await errorHandler.handle(error);
 });
