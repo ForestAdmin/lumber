f65d93f75d1de638cb0010555afd8ce7b9e19fa2
Nicolas SAILLY <nsailly@live.fr>
2021-02-22 15:54:11 +0100

    chore: merge master (#546)

--- services/dumper.js
+++ services/dumper.js
@@ -103,7 +103,7 @@ class Dumper {
       dotenv: '~6.1.0',
       express: '~4.17.1',
       'express-jwt': '5.3.1',
-      [`forest-express-${orm}`]: '^7.0.0-beta.1',
+      [`forest-express-${orm}`]: '^7.0.0',
       morgan: '1.9.1',
       'require-all': '^3.0.0',
       sequelize: '~5.15.1',
