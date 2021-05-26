b5c50c9298ef1700280360b4c2449510f3dcd639
Arnaud Moncel <arnaud.moncel43@gmail.com>
2021-05-26 14:49:19 +0200

    fix: express-jwt vulnerability

--- services/dumper.js
+++ services/dumper.js
@@ -91,7 +91,7 @@ class Dumper {
       debug: '~4.0.1',
       dotenv: '~6.1.0',
       express: '~4.17.1',
-      'express-jwt': '5.3.1',
+      'express-jwt': '6.0.0',
       [`forest-express-${orm}`]: '^7.0.0',
       morgan: '1.9.1',
       'require-all': '^3.0.0',
