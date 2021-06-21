0a5a793ae92862c7b97d88b0bf1a2e3bbaa6c2ca
Sliman <smedini@gmail.com>
2021-06-21 12:00:47 +0200

    fix(tests): wrapper quotes function

--- test/handlebars/helpers/wrap-quotes.unit.test.js
+++ test/handlebars/helpers/wrap-quotes.unit.test.js
@@ -2,10 +2,10 @@ const wrapQuotes = require('../../../handlerbars/helpers/wrap-quotes');
 
 describe('wrap-quotes', () => {
   it('should wrap', () => {
-    expect.assertions(5);
-    expect(wrapQuotes('notSpecialName')).toBe('notSpecialName');
-    expect(wrapQuotes('notSpecialName12')).toBe('notSpecialName12');
-    expect(wrapQuotes('_notSpecialName12')).toBe('_notSpecialName12');
+    expect.assertions(8);
+    expect(wrapQuotes('notSpecialName')).toBe('\'notSpecialName\'');
+    expect(wrapQuotes('notSpecialName12')).toBe('\'notSpecialName12\'');
+    expect(wrapQuotes('_notSpecialName12')).toBe('\'_notSpecialName12\'');
 
     expect(wrapQuotes('special Name')).toBe('\'special Name\'');
     expect(wrapQuotes(' Name')).toBe('\' Name\'');
