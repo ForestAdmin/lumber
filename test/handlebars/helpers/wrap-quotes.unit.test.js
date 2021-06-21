0fc0e74a0e6c821c62e139cc4eedff648447a681
Sliman <smedini@gmail.com>
2021-06-21 11:37:54 +0200

    fix: always wrap with quotes

--- test/handlebars/helpers/wrap-quotes.unit.test.js
+++ test/handlebars/helpers/wrap-quotes.unit.test.js
@@ -1,14 +1,12 @@
 const wrapQuotes = require('../../../handlerbars/helpers/wrap-quotes');
 
 describe('wrap-quotes', () => {
-  it('should not wrap', () => {
-    expect.assertions(3);
+  it('should wrap', () => {
+    expect.assertions(5);
     expect(wrapQuotes('notSpecialName')).toBe('notSpecialName');
     expect(wrapQuotes('notSpecialName12')).toBe('notSpecialName12');
     expect(wrapQuotes('_notSpecialName12')).toBe('_notSpecialName12');
-  });
-  it('should wrap', () => {
-    expect.assertions(5);
+
     expect(wrapQuotes('special Name')).toBe('\'special Name\'');
     expect(wrapQuotes(' Name')).toBe('\' Name\'');
     expect(wrapQuotes('special-name')).toBe('\'special-name\'');
